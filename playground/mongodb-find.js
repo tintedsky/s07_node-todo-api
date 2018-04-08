e3c3f7958f4a1842b4702717e369a6c1f84e3fd1
tintedsky <hongbonew@gmail.com>
2018-04-08 18:47:50 -0400

    find uncompleted task

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server...')
 
   const db = client.db();
-  db.collection('Todos').find().toArray().then(
+  db.collection('Todos').find({completed: false}).toArray().then(
     (docs) => {
       console.log('Todos');
       console.log(JSON.stringify(docs, undefined, 2));
