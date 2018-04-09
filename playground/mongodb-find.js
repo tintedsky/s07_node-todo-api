e67fe9e1de7e4a43b723e9c3770dd1c57a53c7a2
tintedsky <hongbonew@gmail.com>
2018-04-08 22:35:49 -0400

    get count hands on

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,13 +9,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server...')
 
   const db = client.db();
-  db.collection('Todos').find().count()
+  // db.collection('Todos').find({name:'Hong'}).count()
+  //   .then((count) => {
+  //     console.log(`Todos count: ${count}`);
+  //   }, (err) => {
+  //     console.log('Unable to fetch todos', err);
+  //   }
+  // );
+
+  db.collection('Users').find({name:'Moose'}).count()
     .then((count) => {
-      console.log(`Todos count: ${count}`);
+      console.log(`Users count with: ${count}`);
     }, (err) => {
-      console.log('Unable to fetch todos', err);
-    }
-  );
+      console.log('Unable to fetch Users');
+    });
 
   client.close();
 });
