9cee996d6e972a613de8f643488ccea138c3ccc1
tintedsky <hongbonew@gmail.com>
2018-04-21 22:27:31 -0400

    Add POST /users route and token method

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,27 +1,14 @@
 const {SHA256} = require('crypto-js');
-
-var message = "I like moose.";
-console.log(message);
-console.log(SHA256(message).toString());
+const jwt = require('jsonwebtoken');
 
 var data = {
-  id:4
+  id: 17
 };
 
-var salt = 'somexSalt';
 
-var token = {
-  data,
-  hash:SHA256(JSON.stringify(data) + salt).toString()
-};
+var token = jwt.sign(data, '123abc');
 
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded:', decoded);
 
-var resultHash = SHA256(JSON.stringify(data) + salt).toString();
-
-if(resultHash === token.hash){
-  console.log('data not manipulated.');
-}else{
-  console.log('Data was changed. Do not trust.')
-}
+console.log(token);
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const validator = require('validator');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email:{
     type:String,
     required:true,
@@ -28,8 +30,28 @@ var User = mongoose.model('User', {
       required:true
     }
   }]
-
-
 });
+
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  //user.tokens.push({access, token});
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
