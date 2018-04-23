ec9a6a124d96662e393b5c1cededa0f1f8bfc355
tintedsky <hongbonew@gmail.com>
2018-04-22 21:14:48 -0400

    hashing password playground

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 17
-};
+var password = 'abc123!';
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
+var hashedPassword = '$2a$10$/YDBGiwFYe5JN8vBJ3XnZ.Kt2RdvUI9FokWVA0oS.ZQcYTT2bJFem';
 
-var token = jwt.sign(data, '123abc');
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
-
-console.log(token);
+bcrypt.compare('abc123', hashedPassword, (err, res) => {
+  console.log(res);
+});
+//
+// var data = {
+//   id: 17
+// };
+//
+//
+// var token = jwt.sign(data, '123abc');
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
+//
+// console.log(token);
