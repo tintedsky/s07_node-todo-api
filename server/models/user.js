dc365f64519ea436d8ff33ae8c696c250c1e4bc0
tintedsky <hongbonew@gmail.com>
2018-04-23 21:53:58 -0400

    1. get('/users/me'); # Do not forget the leading / 2. 'GET /users/me'

--- server/models/user.js
+++ server/models/user.js
@@ -79,7 +79,7 @@ UserSchema.pre('save', function(next){
   var user = this;
 
   if(user.isModified('password')){
-    bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;  //Hongbo missing
         next();                //Hongbo missing
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -162,3 +162,27 @@ describe('PATCH /todos/:id', () => {
        .end(done);
   });
 });
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
