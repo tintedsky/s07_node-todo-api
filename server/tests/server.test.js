55667e932cabb84807184d0729e2d76204e2371a
tintedsky <hongbonew@gmail.com>
2018-04-27 15:32:51 -0400

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -298,3 +298,22 @@ describe('POST /users/login', () => {
       })
   });
 });
+
+describe('DELETE /users/me/token', ()=>{
+  it('should remove token of the specific user', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e)=>done(e));
+      });
+  });
+});
